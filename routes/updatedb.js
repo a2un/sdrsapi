f5ca25b0a12ddd1a4044c04e4af24e8e03723c11
a2un <encodedgeek@gmail.com>
2020-01-28 01:03:52 -0500

    heroku config vars

--- routes/updatedb.js
+++ routes/updatedb.js
@@ -4,16 +4,16 @@ var router = express.Router();
 var ETFTickers = require('../models/ETFTickers');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-
+router.post('/', function(req, res, next) {
+   
   ETFTickers.sync().then(() => {
     ETFTickers.create(
     {
-      Holdings: 'test2',
-      Country_Weights: 23,
-      Sector_Weights: 23,
-      ETF_name: 'test-ticker2',
-      Fund_Descr: 'this is test inside route'
+      Holdings: req.Holdings,
+      Country_Weights: req.Country_Weights,
+      Sector_Weights: req.Sector_Weights,
+      ETF_name: req.ETF_name,
+      Fund_Descr: req.Fund_Descr
     });
   });
 
